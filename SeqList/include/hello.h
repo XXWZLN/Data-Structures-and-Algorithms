fa864d687c598af8b60e10ad5424a0ae7bec0fc6
XXWLZLN <2829071288@qq.com>
2022-09-08 10:46:48 +0800

    cpp ignored

---
+++ SeqList/include/hello.h
@@ -0,0 +1,13 @@
+/********************************************************************************
+* @author: Antarctic Polar Bear
+* @date: 2022/9/7 13:42
+* describe: none
+********************************************************************************/
+
+
+#ifndef DATA_STRUCTURES_AND_ALGORITHMS_HELLO_H
+#define DATA_STRUCTURES_AND_ALGORITHMS_HELLO_H
+#include "iostream"
+
+void ph();
+#endif//DATA_STRUCTURES_AND_ALGORITHMS_HELLO_H
